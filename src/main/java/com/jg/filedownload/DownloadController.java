f57f26e20f700053e954a5920e3eaa1af55d61d0
Joseph Grech <josephgrech90@gmail.com>
2021-05-30 12:56:22 +0200

    Cleaning.

--- src/main/java/com/jg/filedownload/DownloadController.java
+++ src/main/java/com/jg/filedownload/DownloadController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,61 +18,53 @@ import java.io.InputStream;
 import java.util.List;
 
 @Slf4j
-@EnableAsync
 @RestController
 @RequiredArgsConstructor
 public class DownloadController {
 
     private final SelfApiClient selfApiClient;
 
+    /**
+     * Retrieves a file from another microservice as a StreamingResponseBody, which is then propagated as the response
+     * of this endpoint.
+     * Header Content-Disposition: required to specify the file being attached.
+     * Header Content-Length: required to specify the file's size in bytes, without which, progress is not shown by browsers.
+     */
     @GetMapping(value = "/bridged/{fileName}")
     public ResponseEntity<StreamingResponseBody> getFileBridged(@PathVariable final String fileName) throws IOException {
         log.debug("getFileBridged start.");
         final Response fileResponse = selfApiClient.getFile(fileName);
         final InputStream fileInputStream = fileResponse.body().asInputStream();
-
-        final StreamingResponseBody streamingResponseBody = outputStream -> {
-            int nRead;
-            byte[] data = new byte[1024];
-
-            while ((nRead = fileInputStream.read(data, 0, data.length)) != -1) {
-                log.trace("getFileBridged write...");
-                outputStream.write(data, 0, nRead);
-            }
-
-            fileInputStream.close();
-        };
-
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .contentLength(Long.parseLong(((List<String>) fileResponse.headers().get(HttpHeaders.CONTENT_LENGTH)).get(0)))
-                .body(streamingResponseBody);
+                .body(getStreamingResponseBody(fileInputStream));
     }
 
+    /**
+     * Retrieves a File's content by streaming from FileInputStream to StreamingResponseBody as a response.
+     * Header Content-Disposition: required to specify the file being attached.
+     * Header Content-Length: required to specify the file's size in bytes, without which, progress is not shown by browsers.
+     */
     @GetMapping(value = "/{fileName}")
     public ResponseEntity<StreamingResponseBody> getFile(@PathVariable final String fileName) throws IOException {
         log.debug("getFile start.");
         final File file = new File("files/" + fileName);
         final FileInputStream fileInputStream = new FileInputStream(file);
-
-        final StreamingResponseBody streamingResponseBody = outputStream -> {
-            int nRead;
-            byte[] data = new byte[1024];
-
-            while ((nRead = fileInputStream.read(data, 0, data.length)) != -1) {
-                log.trace("getFileBridged write...");
-                outputStream.write(data, 0, nRead);
-            }
-
-            fileInputStream.close();
-        };
-
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .contentLength(file.length())
-                .body(streamingResponseBody);
+                .body(getStreamingResponseBody(fileInputStream));
+    }
+
+    private StreamingResponseBody getStreamingResponseBody(final InputStream fileInputStream) {
+        log.debug("Generating StreamingResponseBody for transferring InputStream to OutputStream.");
+        return outputStream -> {
+            fileInputStream.transferTo(outputStream);
+            fileInputStream.close();
+        };
     }
 
 }
--- src/main/java/com/jg/filedownload/SelfApiClient.java
+++ src/main/java/com/jg/filedownload/SelfApiClient.java
@@ -4,6 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Feign client to simulate fetching a StreamingResponseBody from another microservice (even though this Feign client
+ * point to this same microservice).
+ */
 @FeignClient(name = "self-api-client", url = "http://localhost:${server.port}")
 public interface SelfApiClient {
 
