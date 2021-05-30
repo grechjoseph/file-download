ad6fedb4b84b6c71af363daa9d9d00eaab8f18e6
Joseph Grech <josephgrech90@gmail.com>
2021-05-30 11:03:57 +0200

    Refactored to use StreamingResponseBody and have Content-Length.

--- src/main/java/com/jg/filedownload/DownloadController.java
+++ src/main/java/com/jg/filedownload/DownloadController.java
@@ -3,16 +3,20 @@ package com.jg.filedownload;
 import feign.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 @Slf4j
 @EnableAsync
@@ -23,36 +27,53 @@ public class DownloadController {
     private final SelfApiClient selfApiClient;
 
     @GetMapping(value = "/bridged/{fileName}")
-    public StreamingResponseBody getFileBridged(@PathVariable final String fileName,
-                                                final HttpServletResponse response) throws FileNotFoundException {
+    public ResponseEntity<StreamingResponseBody> getFileBridged(@PathVariable final String fileName) throws IOException {
         log.debug("getFileBridged start.");
         final Response fileResponse = selfApiClient.getFile(fileName);
-        return outputStream -> {
+        final InputStream fileInputStream = fileResponse.body().asInputStream();
+
+        final StreamingResponseBody streamingResponseBody = outputStream -> {
             int nRead;
             byte[] data = new byte[1024];
 
-            while ((nRead = fileResponse.body().asInputStream().read(data, 0, data.length)) != -1) {
+            while ((nRead = fileInputStream.read(data, 0, data.length)) != -1) {
                 log.trace("getFileBridged write...");
                 outputStream.write(data, 0, nRead);
             }
+
+            fileInputStream.close();
         };
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(Long.parseLong(((List<String>) fileResponse.headers().get(HttpHeaders.CONTENT_LENGTH)).get(0)))
+                .body(streamingResponseBody);
     }
 
     @GetMapping(value = "/{fileName}")
-    public StreamingResponseBody getFile(@PathVariable final String fileName,
-                                         final HttpServletResponse response) throws FileNotFoundException {
+    public ResponseEntity<StreamingResponseBody> getFile(@PathVariable final String fileName) throws IOException {
         log.debug("getFile start.");
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-        final InputStream inputStream = new FileInputStream("files/" + fileName);
-        return outputStream -> {
+        final File file = new File("files/" + fileName);
+        final FileInputStream fileInputStream = new FileInputStream(file);
+
+        final StreamingResponseBody streamingResponseBody = outputStream -> {
             int nRead;
             byte[] data = new byte[1024];
 
-            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                log.trace("getFile write...");
+            while ((nRead = fileInputStream.read(data, 0, data.length)) != -1) {
+                log.trace("getFileBridged write...");
                 outputStream.write(data, 0, nRead);
             }
+
+            fileInputStream.close();
         };
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(file.length())
+                .body(streamingResponseBody);
     }
 
 }
